c59029655f6f81889e4244180006a39dcbe2f854
Steven-Russell <zhangwd159357@163.com>
2020-02-15 11:28:41 +0800

    创建controller

---
+++ security_springboot_coding/src/main/java/com/wd/SecuritySpringbootCodingApplication.java
@@ -0,0 +1,13 @@
+package com.wd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringbootCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySpringbootCodingApplication.class, args);
+    }
+
+}
