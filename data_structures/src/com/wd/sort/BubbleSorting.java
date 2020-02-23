bc622eec67cd8de9229408330ae5936b7e547a3e
Steven-Russell <zhangwd159357@163.com>
2020-02-23 15:47:56 +0800

    查询和搜索基础算法练习

--- data_structures/src/com/wd/sort/BubbleSorting.java
+++ data_structures/src/com/wd/sort/BubbleSorting.java
@@ -1,11 +1,25 @@
 package com.wd.sort;
 
+import java.util.Arrays;
+
 public class BubbleSorting {
     public static void main(String[] args) {
-
+        int[] arr = {1,22,44,33,5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(){
-
+    public static void bubbleSort(int[] arr){
+        int temp = 0;
+        for(int i=0;i<arr.length;){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+                i++;
+            }
+        }
     }
 }
